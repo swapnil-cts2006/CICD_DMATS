fb714564a882392d6f006851bea4d43fcc0ccaa2
Swapnil <swapnil.cts2006@gmail.com>
2021-04-23 22:17:13 +0530

    Initial Code

---
+++ src/main/java/com/example/dmats/DmatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dmats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dmats/DmatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dmats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
